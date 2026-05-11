7a29623f779acee51458be83ed286dae6dc31854
Eric <eric@eric.ad>
2026-05-11 18:41:06 +0200

    Accidental remove

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,6 +1,13 @@
 return {
+  { "williamboman/mason.nvim", opts = {} },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {},
+  },
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" },
     opts = {
       servers = {
         lua_ls = {},
@@ -12,6 +19,18 @@ return {
         vim.lsp.config(server, config)
         vim.lsp.enable(server)
       end
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      })
+      opts.sources = { { name = "nvim_lsp" } }
     end,
   },
 }
